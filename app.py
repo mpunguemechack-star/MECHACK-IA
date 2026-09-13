a3d10416fd03d6584b19515dab400c8e497c1eb6
Mechack Mpungue <mpunguemechack@gmail.com>
2026-09-13 12:29:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ else:
         with st.chat_message("assistant"):
             try:
                 stream = client.chat.completions.create(
-                    model="llama3-8b-8192",
+                    model="llama-3.3-70b-versatile",
                     messages=st.session_state.messages,
                     stream=False,
                 )
